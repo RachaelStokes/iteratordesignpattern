25e46c569f4c78e7bb9eaad77700443945fa262b
Rachael Stokes <stokesrachaele@gmail.com>
2021-02-11 17:55:33 -0500

    minor changes

---
+++ iteratordesignpattern/Item.java
@@ -0,0 +1,26 @@
+package iteratordesignpattern;
+
+public class Item {
+    private String title;
+    private String description;
+    private double price;
+
+    public Item(String title, String description, double price) {
+        this.title = title;
+        this.description = description;
+        this.price = price;
+    }
+
+    public String toString() {
+        
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void print() {
+
+    }
+    
+}
