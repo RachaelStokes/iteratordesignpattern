7fe029ee180bd8fc3480ed577043ffbb41bebd02
Rachael Stokes <stokesrachaele@gmail.com>
2021-02-12 22:21:47 -0500

    added wishlist class

---
+++ iteratordesignpattern/WishList.java
@@ -0,0 +1,28 @@
+package iteratordesignpattern;
+
+public class WishList {
+    private String name;
+    private Item[] items;
+    private int count;
+
+    public WishList(String name) {
+
+    }
+
+    public void addItem(String title, String description, double price) {
+
+    }
+
+    public WishListIterator createIterator() {
+
+    }
+
+    public double getTotalCost() {
+
+    }
+
+    private Item[] growArray(Item[] items) {
+        
+    }
+    
+}
