ade1a238c847ded0b04aa262400d3818f6485b39
Rachael Stokes <stokesrachaele@gmail.com>
2021-02-13 09:31:08 -0500

    added javadoc to wishlist

--- iteratordesignpattern/WishList.java
+++ iteratordesignpattern/WishList.java
@@ -1,15 +1,28 @@
 package iteratordesignpattern;
-
+/**
+ * Method creates a wishlist array of items and adds new items to the array 
+ * @author Rachael
+ */
 public class WishList {
-    private String name;
-    private Item[] items;
-    private int count = 0;
+    private String name; //string name of the wishlist
+    private Item[] items; //array of Item called items
+    private int count = 0; //int count of items in the array
 
+    /**
+     * constructor sets the name of the wishlist and creates array items with a size of 5
+     * @param name string name of wish list
+     */
     public WishList(String name) {
         this.name = name; 
         items = new Item[5];
     }
 
+    /**
+     * method add item only adds an item if there is space in the array
+     * @param title string title of the new item being added
+     * @param description string description of the new item being added
+     * @param price double price of the new item being added
+     */
     public void addItem(String title, String description, double price) {
         Item item = new Item(title, description, price);
         if(count >= 5) {
@@ -20,10 +33,18 @@ public class WishList {
         }
     }
 
+    /**
+     * creates a new iterator for wishlist
+     * @return new wishlistIterator for items array
+     */
     public WishListIterator createIterator() {
         return new WishListIterator(items);
     }
 
+    /**
+     * method that returns the total cost of all the items after they are added up
+     * @return the total cost of all items
+     */
     public double getTotalCost() {
         double price = 0;
         for(int i = 0; i < items.length; i++) {
@@ -32,6 +53,11 @@ public class WishList {
         return price;
     }
 
+    /**
+     * private method grows the array to twice its size
+     * @param items the items going into the grown array
+     * @return returns the new bigger array with all of the items
+     */
     private Item[] growArray(Item[] items) {
         Item[] temp = new Item[2*items.length];
         for(int i = 0; i < temp.length; i++) {
