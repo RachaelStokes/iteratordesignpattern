58d2fafe1e1d047c782f042dfd0700a72aac0f5e
Rachael Stokes <stokesrachaele@gmail.com>
2021-02-12 22:29:10 -0500

    added Wishlistiterator

---
+++ iteratordesignpattern/WishListIterator.java
@@ -0,0 +1,20 @@
+package iteratordesignpattern;
+
+import java.util.Iterator;
+
+public class WishListIterator implements Iterator {
+    private Item[] items;
+    private int position;
+
+    public WishListIterator(Item[] items) {
+        this.items = items;
+    }
+
+    public boolean hasNext() {
+        
+    }
+
+    public Item next() {
+
+    }
+}
